2772188e055dcb5dc17e798b50fc96370acd89c7
Darktt <pippipig@gmail.com>
2025-11-05 19:09:05 +0800

    增加 RequestItem 處理自訂回傳內容

--- Fluxo/Model/Setting.swift
+++ Fluxo/Model/Setting.swift
@@ -16,12 +16,58 @@ struct Setting
     public
     var port: UInt16
     
+    public private(set)
+    var requestItems: Array<RequestItem> = [] {
+        
+        willSet {
+            
+            do {
+                
+                let data: Array<Data> = try newValue.map({ try $0.encode() })
+                
+                self.requestItemDates = data
+            } catch {
+                
+                print("Failed to encode requestItems: \(error)")
+            }
+        }
+    }
+    
+    @UserDefaultsWrapper("requestItemDates", defaultValue: [])
+    private
+    var requestItemDates: Array<Data>
+    
     // MARK: - Methods -
     // MARK: Initial Method
     
     public
     init()
     {
+        self.requestItems = []
         
+        do {
+            
+            let items = try self.requestItemDates.map {
+                
+                try RequestItem.decode(with: $0)
+            }
+            
+            self.requestItems = items
+        } catch {
+            
+            print("Failed to decode requestItemDates: \(error)")
+        }
+    }
+    
+    public mutating
+    func add(_ item: RequestItem)
+    {
+        self.requestItems.append(item)
+    }
+    
+    public mutating
+    func remove(at index: Int)
+    {
+        self.requestItems.remove(at: index)
     }
 }
